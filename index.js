f5be91de6f1b26129ab77d70a13b3ccf758eafdf
JacquesN <j.nguyen14216@gmai.com>
2021-10-10 16:04:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  
+  res.send('Hello World!')
+})
+
+// option 1
+//  app.get('/', function(req, res){
+//   return res.send('Hello World!');
+// })
+
+// option 2
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
